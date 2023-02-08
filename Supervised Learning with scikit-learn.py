2bd7bf592eef388d71de499854a64d29c9959df9
Nicolle Konai <49760705+nkonai@users.noreply.github.com>
2023-02-08 17:27:53 -0300

    Add files via upload

---
+++ Supervised Learning with scikit-learn.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+##1) Classification
+
+
+# In[ ]:
+
+
+#k-Nearest Neighbors: Fit
+
+
+# In[ ]:
+
+
+# Import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the target variable
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+
+# In[ ]:
+
+
+# Predict the labels for the X_new
+y_pred = knn.predict(X_new)
+
+# Print the predictions for X_new
+print("Predictions: {}".format(y_pred)) 
+
+
+# In[ ]:
+
+
+#Measuring model performance
+
+
+# In[ ]:
+
+
+# Import the module
+from sklearn.model_selection import train_test_split
+
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+
+# In[ ]:
+
+
+#Overfitting and underfitting
+# Create neighbors
+neighbors = np.arange(1, 13)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors=neighbor)
+  
+	# Fit the model
+	knn.fit(X_train, y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
+
+
+# In[ ]:
+
+
+#Visualizing model complexity
+# Add a title
+plt.title("KNN: Varying Number of Neighbors")
+
+# Plot training accuracies
+plt.plot(neighbors, train_accuracies.values(), label="Training Accuracy")
+
+# Plot test accuracies
+plt.plot(neighbors, test_accuracies.values(), label="Testing Accuracy")
+
+plt.legend()
+plt.xlabel("Number of Neighbors")
+plt.ylabel("Accuracy")
+
+# Display the plot
+plt.show()
+
+
+# In[ ]:
+
+
+##Regression
+
+
+# In[ ]:
+
+
+import numpy as np
+
+# Create X from the radio column's values
+X = sales_df['radio'].values
+
+# Create y from the sales column's values
+y = sales_df['sales'].values
+
+# Reshape X
+X.shape
+X = X.reshape(-1,1)
+
+# Check the shape of the features and targets
+print(X.shape,y.shape)
+
+
+# In[ ]:
+
+
+# Import LinearRegression
+from sklearn.linear_model import LinearRegression
+
+# Create the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X, y)
+
+# Make predictions
+predictions = reg.predict(X)
+
+print(predictions[:5])
+
+
+# In[ ]:
+
+
+#Visualizing a linear regression model
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create scatter plot
+plt.scatter(X, y, color="blue")
+
+# Create line plot
+plt.plot(X, predictions, color="red")
+plt.xlabel("Radio Expenditure ($)")
+plt.ylabel("Sales ($)")
+
+# Display the plot
+plt.show()
+
+
+# In[ ]:
+
+
+# Create X and y arrays
+X = sales_df.drop("sales", axis=1).values
+y = sales_df["sales"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Instantiate the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X_train,y_train)
+
+# Make predictions
+y_pred = reg.predict(X_test)
+print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+
+
+# In[ ]:
+
+
+#Regression performance
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
+
+
+# In[ ]:
+
+
+#Cross-validation
+
+# Import the necessary modules
+from sklearn.model_selection import KFold, cross_val_score
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
+
+
+# In[ ]:
+
+
+# Print the mean
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_results, [0.025, 0.975]))
+
+
+# In[ ]:
+
+
+#Regularized regression
+
+
+# In[ ]:
+
+
+# Import Ridge
+from sklearn.linear_model import Ridge
+alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+ridge_scores = []
+for alpha in alphas:
+  
+  # Create a Ridge regression model
+  ridge = Ridge(alpha=alpha)
+  
+  # Fit the data
+  ridge.fit(X_train,y_train)
+  
+  # Obtain R-squared
+  score = ridge.score(X_test,y_test)
+  ridge_scores.append(score)
+print(ridge_scores)
+
+
+# In[ ]:
+
+
+# Import Lasso
+from sklearn.linear_model import Lasso
+
+# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.3)
+
+# Fit the model to the data
+lasso.fit(X,y)
+
+# Compute and print the coefficients
+lasso_coef = lasso.fit(X,y).coef_
+print(lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+plt.xticks(rotation=45)
+plt.show()
+
+
+# In[ ]:
+
+
+#Assessing a diabetes prediction classifier
+
+
+# In[ ]:
+
+
+# Import confusion matrix
+from sklearn.metrics import classification_report, confusion_matrix
+
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the model to the training data
+knn.fit(X_train, y_train)
+
+# Predict the labels of the test data: y_pred
+y_pred = knn.predict(X_test)
+
+# Generate the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#Building a logistic regression model
+
+
+# In[ ]:
+
+
+# Import LogisticRegression
+from sklearn.linear_model import LogisticRegression
+
+# Instantiate the model
+logreg = LogisticRegression()
+
+# Fit the model
+logreg.fit(X_train,y_train)
+
+# Predict probabilities
+y_pred_probs = logreg.predict_proba(X_test)[:, 1]
+
+print(y_pred_probs[:10])
+
+
+# In[ ]:
+
+
+# Import roc_curve
+from sklearn.metrics import roc_curve
+
+# Generate ROC curve values: fpr, tpr, thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_probs)
+
+plt.plot([0, 1], [0, 1], 'k--')
+
+# Plot tpr against fpr
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve for Diabetes Prediction')
+plt.show()
+
+
+# In[ ]:
+
+
+# Import roc_auc_score
+from sklearn.metrics import roc_auc_score
+
+# Calculate roc_auc_score
+print(roc_auc_score(y_test, y_pred_probs))
+
+# Calculate the confusion matrix
+print(confusion_matrix(y_test, y_pred))
+
+# Calculate the classification report
+print(classification_report(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#Hyperparameter tuning with GridSearchCV
+
+
+# In[ ]:
+
+
+#Preprocessing and Pipelines
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
